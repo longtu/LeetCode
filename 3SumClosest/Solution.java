72e1148c7353dcccf579c889c9b1e1543bea7113
Ryan Wang <ryanwang0531@gmail.com>
2013-07-25 01:40:19 -0700

    3Sum Closest First Implementation
    Sort before two pointers
    Use two poiters merge to iterate over all possible solutions
    Use Min check for each iteration

---
+++ 3SumClosest/Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int threeSumClosest(int[] num, int target) {
+        if(num == null || num.length < 3)
+            return 0;
+        Arrays.sort(num);
+        
+
+        int minDiff = Integer.MAX_VALUE:
+        int ret = 0;
+        for (int i = 0 ; i < num.length-1; ++i) {
+            int sum = target - num[i];
+            for(int start = i+1, int end = num.length-1;
+                start < end; ) {
+                int val = num[start] + num[end];
+                if( val == sum) {
+                    return target;
+                }
+                int absVal =(val > sum) ? (val-sum):(sum-val);
+                if(absVal < minDiff) {
+                    ret = val+num[i];
+                    minDiff = absVal;
+                }
+                if( val < sum) {
+                    ++start;      
+                }
+                else{
+                    --end;
+                }
+            }
+        }
+        return ret;
+    }
+}
