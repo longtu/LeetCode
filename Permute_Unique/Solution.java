80c2debda285e40f6269fabf62fc97cf2a4b25de
Ryan Wang <ryanwang0531@gmail.com>
2013-09-01 16:56:35 -0700

    [subject line] add combination sum without duplicates, 0-1 knapsack [Bug input/output] input   output  expected [4,4,2,1,4,2,2,1,3], 6 [[2,4],[1,1,4],[1,2,3],[2,2,2],[1,1,2,2],[1,1,2,2],[1,1,2,2]] [[1,1,2,2],[1,1,4],[1,2,3],[2,2,2],[2,4]]
    [Fix]
    See next commit

--- Permute_Unique/Solution.java
+++ Permute_Unique/Solution.java
@@ -26,5 +26,39 @@ public class Solution {
         }
         return res;
     }
+    //another solution is to sort first, and use recursion and used label:
 }
-
+/*
+1:    vector<vector<int> > permuteUnique(vector<int> &num) {  
+2:      // Start typing your C/C++ solution below  
+3:      // DO NOT write int main() function  
+4:      vector<vector<int> > coll;  
+5:      vector<int> solution;  
+6:      if(num.size() ==0) return coll;  
+7:      vector<int> visited(num.size(), 0);  
+8:      sort(num.begin(), num.end());  
+9:      GeneratePermute(num, 0, visited, solution, coll);  
+10:      return coll;  
+11:    }  
+12:    void GeneratePermute(vector<int> & num, int step, vector<int>& visited, vector<int>& solution, vector<vector<int> >& coll)  
+13:    {  
+14:      if(step == num.size())  
+15:      {  
+16:        coll.push_back(solution);  
+17:        return;  
+18:      }  
+19:      for(int i =0; i< num.size(); i++)  
+20:      {  
+21:        if(visited[i] == 0)  
+22:        {  
+23:          if(i>0 && num[i] == num[i-1] && visited[i-1] ==0)  
+24:            continue;  
+25:          visited[i] = 1;  
+26:          solution.push_back(num[i]);  
+27:          GeneratePermute(num, step+1, visited, solution, coll);  
+28:          solution.pop_back();  
+29:          visited[i] =0;  
+30:        }  
+31:      }  
+32:    }  
+*/
