3e4472ea4f70cf276ed8175c6ae13e680ed5f341
Ryan Wang <ryanwang0531@gmail.com>
2013-09-02 01:42:40 -0700

    [subject line] Fix Restore IP Address [Bug input/output]
    [Fix]
    updates the stop condition;
    only checks for '0' when multiple digits being used

--- RestoreIPAddress/Solution.java
+++ RestoreIPAddress/Solution.java
@@ -1,39 +1,35 @@
 public class Solution {
-        public ArrayList<String> restoreIpAddresses(String s) {
-	        ArrayList<String> res = new ArrayList<String>();
-	        if(s == null || s.isEmpty())
-	            return res;
-	        genIPAddress(s, 0, 0, res, "");
-	        return res;
-	    }
+    public ArrayList<String> restoreIpAddresses(String s) {
+        ArrayList<String> res = new ArrayList<String>();
+        if(s == null || s.isEmpty())
+            return res;
+        genIPAddress(s, 0, 0, res, "");
+        return res;
+    }
 
-	    private void genIPAddress(String s, int step, int index, ArrayList<String> res, String base)    {
-	        if(step == 3){
-	            //index exceeds or not enough
-	            if(index >= s.length() ||
-	                    s.length()-index > 3)
-	                return;
+    private void genIPAddress(String s, int step, int index, ArrayList<String> res, String base)    {
+        if(step == 4){
+            //index exceeds or not enough
+            if(index != s.length())
+                return;
+            res.add( base);
+            return;
+        }
+        //generte base
+        String subBase = base;
+        if(step != 0)
+            subBase += ".";
+        for(int i = 1; i<=3; ++i){
+            //each step cannot start with '0'
+            if(i > 1 && s.charAt(index) == '0')
+                return;
 
-	            String sb = s.substring(index); 
-	            if(sb.charAt(0) == '0')
-	                return; 
-	            res.add( base + "." +sb);
-	            return;
-	        }
-	        //each step cannot start with '0'
-	        if(s.charAt(index) == '0')
-	            return;
-	        //generte base
-	        String subBase = base;
-	        if(step != 0)
-	            subBase += ".";
-	        for(int i = 1; i<=3; ++i){
-	            if(index+i >= s.length())
-	                return;
-	            Integer val = Integer.parseInt(s.substring(index, i+index));
-	            if(val > 255)
-	                return;
-	            genIPAddress(s,step+1,index+i, res, subBase+val);
-	        }
-	    }
+            if(index+i > s.length())
+                return;
+            Integer val = Integer.parseInt(s.substring(index, i+index));
+            if(val > 255)
+                return;
+            genIPAddress(s,step+1,index+i, res, subBase+val);
+        }
+    }
 }
