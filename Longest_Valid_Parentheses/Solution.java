cf523b6ca938e86feb03c726a395e024c2b8e555
Ryan Wang <ryanwang0531@gmail.com>
2013-09-12 00:15:41 -0700

    [subject line] add longest valid parentheses [Bug input/output]
    [Fix]

--- Longest_Valid_Parentheses/Solution.java
+++ Longest_Valid_Parentheses/Solution.java
@@ -1,29 +1,79 @@
-    public int longestValidParentheses(String s) {
-        if(s == null || s.length() == 0)
-            return 0;
+public class Solution {
+	    class Record{
+            //When you propose a compound structure, think about
+            //whether all of the information is needed here or not
+            //val here is actually not being used at all
+	        public int index;
+	        public char val;
+	        public Record(int index, char val){
+	            this.index = index;
+	            this.val = val;
+	        }
+	    }
+	    public int longestValidParentheses(String s) {
+	        if(s == null || s.length() == 0)
+	            return 0;
 
-        char [] arr = s.toCharArray();
-        int count  = 0;
-        int max = 0;
-        for( int start = 0, end = 0; end < arr.length; end++){
-            if(arr[end] == '('){
-                --count; 
-            }
-            else{
-                ++count;
-            }
-            if(count<=0){
-                //this always needs to be updated
-                max = (end+1-start+count > max)?(end+1-start+count):(max);
-            }
-            else{
-                while(end<arr.length && arr[end] == ')')
-                    ++end;
-                count = 0;
-                start = end;
-                //this is very easy to forget
-                end--;
+	        char [] arr = s.toCharArray();
+	        int [] flag = new int[arr.length];
+	        Stack<Record> stack = new Stack<Record>();
+
+	        for( int i = 0; i < arr.length; ++i){
+	            if(arr[i] == '('){
+	                stack.push(new Record(i, arr[i]));
+	            }
+	            else{
+                    //peek will throw stack empty exception
+	                if(stack.isEmpty()){
+	                    continue;
+	                }
+	                Record top = stack.pop();
+	                flag [i] = 1;
+	                flag [top.index] = 1;
+	            }
+	        }
+
+	        int start = 0, end = 0; 
+	        int max = 0;
+	        while(end < flag.length){
+	            while(end < flag.length && flag[end] == 0){
+	                end++;
+	                start++;
+	            }
+	            while(end < flag.length && flag[end] == 1){
+	                end++;
+	            }
+	            int dist = (end - start);
+	            if(dist > max)
+	                max = dist;
+	            start = end;
+	        }
+	        return max;
+	    }
+
+	}
+}
+//another way is to:
+public int longestValidParentheses(String s) {
+    int maxLen = 0, last = -1;
+    Stack<Integer> lefts = new Stack<Integer>();
+    for (int i=0; i<s.length(); ++i) {
+        if (s.charAt(i)=='(') {
+            lefts.push(i);
+        } else {
+            if (lefts.isEmpty()) {
+                // no matching left
+                last = i;
+            } else {
+                // find a matching pair
+                lefts.pop();
+                if (lefts.isEmpty()) {
+                    maxLen = Math.max(maxLen, i-last);
+                } else {
+                    maxLen = Math.max(maxLen, i-lefts.peek());
+                }
             }
         }
-        return max;
     }
+    return maxLen;
+}
