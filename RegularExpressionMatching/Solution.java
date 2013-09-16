c2a4f427029c1c780c98ca15022bb29ce013796e
Ryan Wang <ryanwang0531@gmail.com>
2013-09-16 01:07:17 -0700

    [subject line] add wild card matching [Bug input/output] input   output  expected "", "*" false   true [Fix] see next commit

---
+++ RegularExpressionMatching/Solution.java
@@ -0,0 +1,28 @@
+//wild card matching
+public class Solution {
+    public boolean isMatch(String s, String p) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		if(s == null && p == null)
+			return true;
+		if(s == null)
+			return p.isEmpty();
+		if(p == null)
+			return s.isEmpty();
+		if(s.isEmpty() && p.isEmpty())
+			return true;
+		if(s.isEmpty() || p.isEmpty())
+			return false;
+
+		if(p.charAt(0) == '*'){
+			for(int i =0; i<=s.length();++i){
+				if(isMatch(s.substring(i), p.substring(1)))
+					return true;
+			}
+			return false;
+		}
+		if(p.charAt(0) == '?')
+			return isMatch(s.substring(1), p.substring(1));
+		return (p.charAt(0) == s.charAt(0)) && isMatch(s.substring(1), p.substring(1));
+	}
+}
