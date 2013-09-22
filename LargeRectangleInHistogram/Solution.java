0e3e0792d8da265ece727b5fa5f7a068a5ac9370
Ryan Wang <ryanwang0531@gmail.com>
2013-09-22 15:50:47 -0700

    [subject line] add max rectangulars [Bug input/output]
    [Fix]

--- LargeRectangleInHistogram/Solution.java
+++ LargeRectangleInHistogram/Solution.java
@@ -41,3 +41,22 @@ public class Solution {
     }
 
 }
+
+/*O(N) solution*/
+/*using a stack to keep an increasing height of hists*/
+public int largestRectangleArea2(int[] height) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int i = 0;
+        int maxArea = 0;
+        int[] h = new int[height.length + 1];
+        h = Arrays.copyOf(height, height.length + 1);
+        while(i < h.length){
+            if(stack.isEmpty() || h[stack.peek()] <= h[i]){
+                stack.push(i++);
+            }else {
+                int t = stack.pop();
+                maxArea = Math.max(maxArea, h[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
+            }
+        }
+        return maxArea;
+    }
