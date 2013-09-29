ccfcff5fbe12943a866f7e778feb8f7b45748e9b
Ryan Wang <ryanwang0531@gmail.com>
2013-09-29 01:08:02 -0700

    [subject line] add add Binary [Bug input/output]
    [Fix]

--- AddBinary/Solution.java
+++ AddBinary/Solution.java
@@ -10,17 +10,22 @@ public class Solution {
         int destLen = b.length();
         int maxSize = (srcLen > destLen)?(srcLen):(destLen);
         char [] sum = new char[maxSize+1];
+        //remember to reset!!!
         int carry = 0;
         int i = 0;
         for(; i<maxSize || carry != 0; ++i){
             int tmp = carry;
+            carry = 0;
+            //remember to convert from int to char
             if(i < srcLen)
-                tmp +=src[srcLen-1-i];
+                tmp +=src[srcLen-1-i]-'0';
             if(i < destLen)
-                tmp +=dest[destLen-1-i];
-            if(tmp >1)
+                tmp +=dest[destLen-1-i]-'0';
+            if(tmp >1) {
                 carry = 1;
-            sum[i]= tmp - 0 + '0';
+                tmp -=2;
+            }
+            sum[i]= (char)(tmp - 0 + '0');
         }
         --i;
         for(int j = 0; j<i; j++,i--){
