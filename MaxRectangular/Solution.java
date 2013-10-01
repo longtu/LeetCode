3c5f384263649e35b21970ffc54ab7b1f1188d84
Ryan Wang <ryanwang0531@gmail.com>
2013-10-01 00:34:23 -0700

    [subject line] add max Rectangular and level order traversal [Bug input/output]
    [Fix]

--- MaxRectangular/Solution.java
+++ MaxRectangular/Solution.java
@@ -16,23 +16,21 @@ public class Solution {
     			dp[i][j] = 1;
 
     	int max = 0;
-    	for(int h = 0; h<width; ++h){
+    	for(int h = 0; h<height; ++h){
     		for(int x = 0; x+h < height; ++x){
     			int last = -1;
-    			int maxWidth = 0;
     			for(int y = 0; y<width; ++y){
     				if(dp[x][y] == 1 && matrix[x+h][y] == '1'){
     					if(last == -1)
     						last = y;
     					dp[x][y] = 1;
-    					maxWidth = (maxWidth > y-last+1)?(maxWidth):(y-last+1);
+    					max = (max > (h+1)*(y-last+1))?(max):((h+1)*(y-last+1));
     				}
     				else{
     					last = -1;
     					dp[x][y] = 0;
     				}
     			}
-    			max = (max > maxWidth*(h+1))?(max):(maxWidth*(h+1));
     		}
     	}
     	return max;
