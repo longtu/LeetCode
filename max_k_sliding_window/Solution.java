292c71343b79c7f4eee5be7bfd59f3ca6c5d4136
Ryan Wang <ryanwang0531@gmail.com>
2013-10-04 00:02:13 -0700

    [subject line] add fix [Bug input/output]
    [Fix]

--- max_k_sliding_window/Solution.java
+++ max_k_sliding_window/Solution.java
@@ -1,23 +1,23 @@
 public class Solution {
-
     int [] getKWindowMax(int [] A, int k){
-
+        
         if(A == null || A.length < k)
             return null;
-        int res = new int [A.length - k +1];
+        int [] res = new int [A.length - k +1];
         LinkedList<Integer> deque = new LinkedList<Integer>();
 
         for(int i = 0; i<A.length; ++i){
-            deque.addLast(A[i]);
-            int before = deque.peekFirst();
+            Integer before = deque.peekLast();
             while(before != null && before < A[i]){
-                deque.removeFirst();
-                before = deque.peekFirst();
+                deque.removeLast();
+                before = deque.peekLast();
             }
+            deque.addLast(A[i]);
+
             if(i >= k-1){
-                res[i-k+1] = deque.peekLast();
-                if(A[i-k+1] == deque.peekLast())
-                    deque.removeLast();
+                res[i-k+1] = deque.peekFirst();
+                if(A[i-k+1] == deque.peekFirst())
+                    deque.removeFirst();
             }
         }
         return res;
