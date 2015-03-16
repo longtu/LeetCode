1a5165f8bc83b5557d2e33732cce2c6b47376ee1
wangran <ryanwang0531@gmail.com>
2015-03-16 00:16:23 -0700

    create test for number_of_1_bits

---
+++ algo/src/wr/leetcode/algo/number_of_1_bits/Tester.java
@@ -0,0 +1,10 @@
+package wr.leetcode.algo.number_of_1_bits;
+
+
+public class Tester {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.hammingWeight(1));
+    }
+}
