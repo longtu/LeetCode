cd175a3c7889593db63a363914600555bec6a2a4
wangran <ryanwang0531@gmail.com>
2015-03-21 15:00:04 -0700

    [subject line] add edit distance
    [Bug input/output]
    
    [Fix]

---
+++ algo/src/wr/leetcode/algo/edit_distance/Solution.py
@@ -0,0 +1,35 @@
+class Solution:
+    # @return an integer
+    def minDistance(self, word1, word2):
+   	if word1 is None:
+   		word1 = ""
+   	if word2 is None:
+   		word2 = ""
+
+   	m = len(word1)
+   	n = len(word2)
+
+   	sol = [ [ 0 for y in xrange(n+1)] for i in xrange(2)]
+
+   	for i in xrange(m+1):
+   		for j in xrange(n+1):
+   			sol [i%2][j] = max(i,j);
+   			if i > 0:
+   				sol[i%2][j] = min (sol[i%2][j], sol[(i-1)%2][j] +1)
+   			if j > 0:
+   				sol[i%2][j] = min (sol[i%2][j], sol[i%2][j-1]+1)
+   			if i > 0 and j > 0 :
+   				diff = 1
+   				if word1[(i-1)%2] == word2[j-1]:
+   					diff = 0
+   				sol[i%2][j] = min (sol[i%2][j], sol[(i-1)%2][j-1] + diff)
+
+   	return sol[m%2][n]
+
+if __name__ == '__main__':
+	sol = Solution()
+	print sol.minDistance("", "")
+	print sol.minDistance("a", "")
+	print sol.minDistance("", "ab")
+	print sol.minDistance("baab", "aa")
+	print sol.minDistance("abab", "baba")
