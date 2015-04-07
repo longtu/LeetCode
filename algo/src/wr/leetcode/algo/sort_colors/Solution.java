b54d768feeb230d77536177df90c1fa42d5a2869
wangran <ryanwang0531@gmail.com>
2015-04-06 23:36:39 -0700

    [subject line] First impl of color sort [Bug input/output]
    [Fix]

---
+++ algo/src/wr/leetcode/algo/sort_colors/Solution.java
@@ -0,0 +1,35 @@
+package wr.leetcode.algo.sort_colors;
+
+public class Solution {
+    public void sortColors(int[] A) {
+        if( null == A ) {
+        	return;
+        }
+
+        int next = 0;
+        int firstOne = -1;
+        int end = A.length - 1;
+
+        while(next <= end) {
+        	if(A[next] == 1) {
+        		if(firstOne == -1) {
+        			firstOne = next;
+        		}
+        		next++;
+        	} else if (A[next] == 0) {
+        		if(firstOne != -1){
+        			swap(A, next, firstOne);
+        		}
+        		next++;
+        	} else {
+        		swap(A, next, end--);
+        	}
+        }
+    }
+
+    private void swap(int [] A, int src, int dest) {
+    	int tmp = A[src];
+    	A[src] = A[dest];
+    	A[dest] = tmp;
+    }
+}
