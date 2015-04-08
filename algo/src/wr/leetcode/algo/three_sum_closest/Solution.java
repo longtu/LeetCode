234c0cda00bf8943e4914d9e0aeb6eb5f6210925
wangran <ryanwang0531@gmail.com>
2015-04-08 00:53:55 -0700

    add fix one typo and one weird timeout caused by JDK8 stream reduce

--- algo/src/wr/leetcode/algo/three_sum_closest/Solution.java
+++ algo/src/wr/leetcode/algo/three_sum_closest/Solution.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 public class Solution {
     public int threeSumClosest(int[] num, int target) {
         int ret = 0;
-        if (null == num) {
-            ret = 0;
-        } else if (num.length <= 3) {
-            ret = Arrays.stream(num).reduce(Integer::sum).orElse(0);
+        if (null == num || num.length < 3) {
+            throw new RuntimeException("Invalid Input");
         } else {
             Arrays.sort(num);
             int minDiff = Integer.MAX_VALUE;
@@ -38,7 +36,7 @@ public class Solution {
                 int absDiff = Math.abs(sum-target);
                 if(absDiff < minDiff) {
                     minDiff = absDiff;
-                    ret = target;
+                    ret = sum;
                 }
                 if (sum < target) {
                     start++;
@@ -52,7 +50,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] arr = {-1, 2, 1, -4};
-        System.out.println(sol.threeSumClosest(arr, 1));
+        int[] arr = {43,75,-90,47,-49,72,17,-31,-68,-22,-21,-30,65,88,-75,23,97,-61,53,87,-3,33,20,51,-79,43,80,-9,34,-89,-7,93,43,55,-94,29,-32,-49,25,72,-6,35,53,63,6,-62,-96,-83,-73,66,-11,96,-90,-27,78,-51,79,35,-63,85,-82,-15,100,-82,1,-4,-41,-21,11,12,12,72,-82,-22,37,47,-18,61,60,55,22,-6,26,-60,-42,-92,68,45,-1,-26,5,-56,-1,73,92,-55,-20,-43,-56,-15,7,52,35,-90,63,41,-55,-58,46,-84,-92,17,-66,-23,96,-19,-44,77,67,-47,-48,99,51,-25,19,0,-13,-88,-10,-67,14,7,89,-69,-83,86,-70,-66,-38,-50,66,0,-67,-91,-65,83,42,70,-6,52,-21,-86,-87,-44,8,49,-76,86,-3,87,-32,81,-58,37,-55,19,-26,66,-89,-70,-69,37,0,19,-65,38,7,3,1,-96,96,-65,-52,66,5,-3,-87,-16,-96,57,-74,91,46,-79,0,-69,55,49,-96,80,83,73,56,22,58,-44,-40,-45,95,99,-97,-22,-33,-92,-51,62,20,70,90};
+        System.out.println(sol.threeSumClosest(arr, 284));
+        int[] a1 = {-1, 2};
+        System.out.println(sol.threeSumClosest(a1, 1));
+        int[] a2 = {-1};
+        System.out.println(sol.threeSumClosest(a2, 1));
+        int[] a3 = {};
+        System.out.println(sol.threeSumClosest(a3, 1));
+        int[] a4 = null;
+        System.out.println(sol.threeSumClosest(a4, 1));
     }
 }
