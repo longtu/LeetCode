a627cff958b11d9dfb79dc48747b583a67bfae30
wangran <ryanwang0531@gmail.com>
2015-04-09 00:36:08 -0700

    add parition list

---
+++ algo/src/wr/leetcode/algo/partition_list/Solution.java
@@ -0,0 +1,26 @@
+package wr.leetcode.algo.partition_list;
+
+import wr.leetcode.algo.ListNode;
+
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode bigHead = new ListNode(-1);
+        ListNode big = bigHead;
+        ListNode smallHead = new ListNode(-1);
+        ListNode small = smallHead;
+        small.next = head;
+
+        while(small.next != null){
+            if(small.next.val >= x) {
+                big.next = small.next;
+                small.next = small.next.next;
+                small = small.next;
+                big = big.next;
+                big.next = null;
+            }
+        }
+        small.next = bigHead.next;
+        return smallHead.next;
+    }
+}
