9e908639c5a1aff23ce61c3bd2ad87db18f49cd0
wangran <ryanwang0531@gmail.com>
2015-04-13 23:24:45 -0700

    [subject line] add search rotated sorted array ii [Bug input/output]
    [Fix]

--- algo/src/wr/leetcode/algo/search_in_rotated_sorted_array_ii/Solution.java
+++ algo/src/wr/leetcode/algo/search_in_rotated_sorted_array_ii/Solution.java
@@ -2,31 +2,28 @@ package wr.leetcode.algo.search_in_rotated_sorted_array_ii;
 
 public class Solution {
     public boolean search(int[] A, int target) {
-        if(null == A) {
-            A = new int[0];
-        }
+        int find = -1;
+        if(A == null)
+            return false;
         int start = 0;
-        int end = A.length -1;
-        boolean ret = false;
-
-        while (start <= end){
-            int mid = start + ((end-start)<<1);
-            if(A[mid] == target) {
-                ret = true;
-            } else if (A[mid] >= A[start]) {
-                if(target < A[mid] && target >= A[start]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            } else {
-                if(target > A[mid] && target <= A[end]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
+        int end = A.length-1;
+        while(start<=end){
+            int mid = start + ((end-start)>>1);
+            if(A[mid] == target)
+                return true;
+            if(A[start] < A[mid]){
+                if(target >=A[start] && target < A[mid])
+                    end = mid-1;
+                else
+                    start = mid+1;
             }
+            else if (A[start] > A[mid]){
+                if(target > A[mid] && target <=A[end])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            } else start ++;
         }
-        return ret;
+        return false;
     }
 }
