962b94fe14d570d0523c2ab6f630aa8ad91959be
wangran <ryanwang0531@gmail.com>
2015-04-13 23:39:18 -0700

    [subject line] add pow implementation [Bug input/output] 1.00000, -2147483648 Line 7: java.lang.StackOverflowError
    [Fix]

---
+++ algo/src/wr/leetcode/algo/powx_n/Solution.java
@@ -0,0 +1,35 @@
+package wr.leetcode.algo.powx_n;
+
+public class Solution {
+    public double pow(double x, int n) {
+        double ret;
+        if( 0 == n) {
+            ret = 1;
+        } else if (n < 0) {
+            ret = 1.0/pow(x, 0-n);
+        } else {
+            double half = pow(x, n/2);
+            ret = half * half;
+            if(n%2 == 1){
+                ret *= x;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.pow(2,-2));
+        System.out.println(sol.pow(2,-1));
+        System.out.println(sol.pow(2,0));
+        System.out.println(sol.pow(2,1));
+        System.out.println(sol.pow(2,2));
+        System.out.println(sol.pow(2,3));
+        System.out.println(sol.pow(2,4));
+
+    }
+
+
+
+
+}
