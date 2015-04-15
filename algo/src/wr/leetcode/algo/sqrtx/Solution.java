acdc31ef528f2a1be76229893818c821e78545dc
wangran <ryanwang0531@gmail.com>
2015-04-14 22:20:59 -0700

    First impl of Sqrt(x) [Bug input/output] 5:2 6:3 [Fix]

---
+++ algo/src/wr/leetcode/algo/sqrtx/Solution.java
@@ -0,0 +1,35 @@
+package wr.leetcode.algo.sqrtx;
+
+public class Solution {
+    public int mySqrt(int x) {
+        if(x < 0) {
+            throw new RuntimeException("Invalid Input");
+        }
+        int start = 0;
+        int end = x;
+        int ret = x;
+        while(x > 1 && start <= end) {
+            int mid = start +((end-start)>>1);
+            int left = (mid-1)*(mid-1);
+            int right = (mid+1)*(mid+1);
+            if(left < x && right > x){
+                ret = mid;
+                break;
+            } else if(left >= x){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        for(int i = 0; i < 101; ++i) {
+            System.out.println(i+ ":" + sol.mySqrt(i));
+        }
+    }
+
+
+}
