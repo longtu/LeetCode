25f0e92df5a1d8b5eb6cf9712e4dcc2b49cb5443
wangran <ryanwang0531@gmail.com>
2015-04-19 17:17:05 -0700

    [subject line] add first impl of Reorder list [Bug input/output] Runtime Error Message:  Line 20: java.lang.NullPointerException Last executed input:    {} [Fix]

---
+++ algo/src/wr/leetcode/algo/reorder_list/Solution.java
@@ -0,0 +1,54 @@
+package wr.leetcode.algo.reorder_list;
+
+import wr.leetcode.algo.ListNode;
+
+public class Solution {
+    public void reorderList(ListNode head) {
+        int len = len(head);
+        int join = (len+1)/2;
+        ListNode node = head;
+        for (int i = 1; i < join; ++i) {
+            node = node.next;
+        }
+        ListNode right = reverse(node.next);
+        node.next = null;
+        merge(head, right);
+    }
+
+    public int len (ListNode head) {
+        int len = 0;
+        while(null != head) {
+            head = head.next;
+            len ++;
+        }
+        return len;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode node = new ListNode(-1);
+        while(null != head) {
+            ListNode headNext = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = headNext;
+        }
+        return node.next;
+    }
+
+    public ListNode merge (ListNode left, ListNode right) {
+        ListNode helper = new ListNode(-1);
+        while(null != left || null != right) {
+            if(null != left) {
+                helper.next = left;
+                helper = left;
+                left = left.next;
+            }
+            if(null != right) {
+                helper.next = right;
+                helper = right;
+                right = right.next;
+            }
+        }
+        return helper.next;
+    }
+}
