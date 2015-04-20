1942d6f06fc640cebc9c117704a71a3875a63c63
wangran <ryanwang0531@gmail.com>
2015-04-19 21:14:49 -0700

    [subject line] add Remove Duplicates from Sorted List II [Bug input/output]
    [Fix]

--- algo/src/wr/leetcode/algo/remove_duplicates_from_sorted_list_ii/Solution.java
+++ algo/src/wr/leetcode/algo/remove_duplicates_from_sorted_list_ii/Solution.java
@@ -3,7 +3,7 @@ package wr.leetcode.algo.remove_duplicates_from_sorted_list_ii;
 import wr.leetcode.algo.ListNode;
 
 public class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
+    public ListNode deleteDuplicatesWEAK(ListNode head) {
 
         ListNode helper = new ListNode(-1);
         ListNode n = helper;
@@ -23,6 +23,27 @@ public class Solution {
             node = node.next;
         }
         n.next = null;
+        return helper.next;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode helper = new ListNode(-1);
+        helper.next = head;
+
+        ListNode prev = helper;
+        while(prev.next != null) {
+            ListNode node = prev.next;
+            int count = 0;
+            while(node != null && node.val == prev.next.val){
+                node = node.next;
+                count ++;
+            }
+            if(count != 1) {
+                prev.next = node;
+            } else {
+                prev = prev.next;
+            }
+        }
         return helper.next;
     }
 }
