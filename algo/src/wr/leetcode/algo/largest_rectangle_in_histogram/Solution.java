3e02b80dc5175f562fb1dce0a18e785c522e79df
wangran <ryanwang0531@gmail.com>
2015-04-27 00:43:49 -0700

    [subject line] fix largest rectangular [Bug input/output]
    [Fix]

--- algo/src/wr/leetcode/algo/largest_rectangle_in_histogram/Solution.java
+++ algo/src/wr/leetcode/algo/largest_rectangle_in_histogram/Solution.java
@@ -13,16 +13,10 @@ public class Solution {
         int maxArea = 0;
         for (int i = 0; i < height.length; ++i) {
             if(!stack.isEmpty() && height[stack.peek()] > height[i]) {
-                int j = 0;
-                while(!stack.isEmpty() ){
-                    j = stack.peek();
-                    maxArea = Math.max(maxArea, (i-j)*height[j]);
-                    if(height[j] <= height[i]){
-                        break;
-                    }
-                    stack.pop();
+                while(!stack.isEmpty() && height[stack.peek()] > height[i]){
+                    int j = stack.pop();
+                    maxArea = Math.max(maxArea, (i-stack.peek()-1)*height[j]);
                 }
-                maxArea = Math.max(maxArea, (i-j+1)*height[i]);
             }
             stack.push(i);
         }
@@ -32,10 +26,14 @@ public class Solution {
     public static void main(String[] args) {
         int[] a = {2,1,5,6,2,3};
         int[] b = {2,1,2};
-        int[] c = {2,1,2};
+        int[] c = {4,2};
+        int[] d = {6,5,4};
 
         Solution sol = new Solution();
         System.out.println(sol.largestRectangleArea(a));
         System.out.println(sol.largestRectangleArea(b));
+        System.out.println(sol.largestRectangleArea(c));
+        System.out.println(sol.largestRectangleArea(d));
+
     }
 }
