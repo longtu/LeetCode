4dc0fab906c9ffd43302b746ef85d2d1739b5330
Ryan <ryanwang0531@gmail.com>
2015-10-07 23:53:57 -0700

    update add_two_numbers solution
    add notes for review

--- algo/src/wr/leetcode/algo/add_two_numbers/Solution.java
+++ algo/src/wr/leetcode/algo/add_two_numbers/Solution.java
@@ -11,6 +11,9 @@ public class Solution {
         ListNode helper = new ListNode(-1);
         ListNode node = helper;
         int carry = 0;
+        /**
+         * BUG: you might have missed edge case : carry > 0
+         */
         while(left != null || right != null || carry > 0) {
             int sum = carry;
             if(null != left){
@@ -24,42 +27,6 @@ public class Solution {
             carry = sum/10;
             node.next = new ListNode(sum%10);
             node = node.next;
-        }
-        return helper.next;
-    }
-
-    public ListNode addTwoNumbers0(ListNode l1, ListNode l2) {
-        ListNode left = reverse(l1);
-        ListNode right = reverse(l2);
-
-        ListNode helper = new ListNode(-1);
-        ListNode node = helper;
-        int carry = 0;
-        while(left != null || right != null) {
-            int sum = carry;
-            if(null != left){
-                sum += left.val;
-                left = left.next;
-            }
-            if (null != right) {
-                sum += right.val;
-                right = right.next;
-            }
-            carry = sum/10;
-            node.next = new ListNode(sum%10);
-            node = node.next;
-        }
-        return helper.next;
-    }
-
-    public ListNode reverse (ListNode head) {
-        ListNode helper = new ListNode(-1);
-        helper.next = null;
-        while(head != null) {
-            ListNode headNext = head.next;
-            head.next = helper.next;
-            helper.next = head;
-            head = headNext;
         }
         return helper.next;
     }
