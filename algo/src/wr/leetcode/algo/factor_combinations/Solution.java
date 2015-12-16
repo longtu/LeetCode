2e9b81df0134b13a155e3a21569246e009bb6f9f
wangran <ryanwang0531@gmail.com>
2015-12-16 00:04:12 -0800

    add factor combinations

--- algo/src/wr/leetcode/algo/factor_combinations/Solution.java
+++ algo/src/wr/leetcode/algo/factor_combinations/Solution.java
@@ -4,10 +4,14 @@ import java.util.*;
 
 public class Solution {
 
-    //Method1: recursive, TLE Solution
-    public List<List<Integer>> getFactors0(int n) {
-        Map<Integer, Set<List<Integer>>> dp = new HashMap<>(n);
+    public List<List<Integer>> getFactors(int n) {
+        //MLE: HashMap(n) -> HashMap()
+        Map<Integer, Set<List<Integer>>> dp = new HashMap<>();
         for (int i = 4; i <= n; ++i) {
+            //TLE: Skip the ones are not dividable
+            if( 0 != n % i ) {
+                continue;
+            }
             Set<List<Integer>> factorCombinations = new HashSet<>();
             for( int j = 2; j * 2 <= i; ++j) {
                 if( 0 == i%j) {
@@ -39,47 +43,11 @@ public class Solution {
         return new LinkedList<>(ret);
     }
 
-    //Method2:
-    public List<List<Integer>> getFactors(int n) {
-
-        int[] primes = primes(n/2);
-        int m = primes.length;
-        Map<Integer, Integer> factors = new LinkedHashMap<>();
-        for (int i = 0; i < m; ++i) {
-            int prime = primes[i];
-            int factor = prime;
-            int count = 0;
-            while(factor < n && 0 == (n % factor)) {
-                count += 1;
-                factor *= prime;
-            }
-            if( count > 0 ) {
-                factors.put(prime, count);
-            }
-        }
-        return null;
-
-    }
-
-    public Set<List<Integer>> factors (LinkedHashMap<Integer, Integer> factors) {
-        Set<List<Integer>> ret = new HashSet<>();
-        return ret;
-    }
-
-
-    //primes smaller or equal to n
-    public int[] primes( int n) {
-        List<Integer> ret = new LinkedList<>();
-        return null;
-
-    }
-
-
     public static void main(String[] args) {
         Solution sol = new Solution();
 
         for (int i : new int[] {
-                1,37,12,32, 62847
+                /*1,37,12,32, */62847, 23848713
         }) {
             System.out.println(sol.getFactors(i));
         }
